cd5f6f5dc8949b04ee26e8e344f2749edbf69f25
Pi-Bouf <pierre.bouffier05@gmail.com>
2018-06-15 21:39:56 +0200

    Fixing TNetLib compilation

--- TServer/TNetLib/stdafx.h
+++ TServer/TNetLib/stdafx.h
@@ -1,22 +1,17 @@
-// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
-// 표준 시스템 포함 파일 및 프로젝트 관련 포함 파일이 
-// 들어 있는 포함 파일입니다.
-//
-
 #pragma once
 
-#ifndef _WIN32_WINNT		// Windows NT 4 이상에서만 기능을 사용할 수 있습니다.
-#define _WIN32_WINNT 0x0500	// 이 값을 Windows 2000 이상을 대상으로 하는 데 적합한 값으로 변경하십시오.
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0502
 #endif		
 
 #pragma warning( disable : 4786 4530 4503 4552)
 
 
-#define WIN32_LEAN_AND_MEAN		// 거의 사용되지 않는 내용은 Windows 헤더에서 제외합니다.
-#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// 일부 CString 생성자는 명시적으로 선언됩니다.
+#define WIN32_LEAN_AND_MEAN
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS
 
 #ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN		// 거의 사용되지 않는 내용은 Windows 헤더에서 제외합니다.
+#define VC_EXTRALEAN
 #endif
 
 #include <TNetLib.h>
