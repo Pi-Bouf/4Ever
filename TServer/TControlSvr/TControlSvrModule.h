331c662985b051417276ed0621976e1f8188a6ee
Pi-Bouf <pierre.bouffier05@gmail.com>
2018-07-21 00:35:31 +0200

    Adding TControlSvr

--- TServer/TControlSvr/TControlSvrModule.h
+++ TServer/TControlSvr/TControlSvrModule.h
@@ -2,19 +2,9 @@
 
 #define ON_RECEIVE(p)							case p : return On##p(pBUF);
 
-////////////////////// 매우매우 중요 - 소켓 종료요령 /////////////////////////////////////
-//
-// 1. 자기자신을 종료 하려면 핸들러함수에서 EC_SESSION_INVALIDCHAR를 리턴한다.
-//    (핸들러 함수의 파라메터로 넘어온 소켓 pBUF->m_pSESSION이 종료)
-//
-// 2. 다른 소켓을 종료 하려면 종료대상을 파라메터로 하여 CloseSession()을 호출한다.
-//    (예 : CloseSession(pTarget); pTarget이 스스로 종료하도록 유도된다)
-//
-// 3. 이 이외의 방법으로 절대 소켓을 종료하면 안된다.
-//
-//////////////////////////////////////////////////////////////////////////////////////////
+#include "TServerSystem.h"
 
-class CTControlSvrModule : public CAtlServiceModuleT< CTControlSvrModule, IDS_SERVICENAME >
+class CTControlSvrModule : public TServerSystem
 {
 protected:
 	QPACKETBUF m_qBATCHJOB;					// queue for batch job
@@ -43,17 +33,16 @@ protected:
 	FILE * m_fileTemp;
 
 protected:
-	TCHAR m_szDBUserID[ONE_KBYTE];
-	TCHAR m_szDBPasswd[ONE_KBYTE];
-	TCHAR m_szDSN[ONE_KBYTE];
+	string m_szDBUserID;
+	string m_szDBPasswd;
+	string m_szDSN;
+	int m_wPort;
 
 	BYTE m_bNumWorker;
 	BYTE m_bThreadRun;
 	BYTE m_bBatchRun;
 	BYTE m_bSMRun;
 	BYTE m_bTimerRun;
-
-	WORD m_wPort;
 
 	HANDLE m_hWorker[MAX_THREAD];
 	HANDLE m_hControl;
@@ -227,173 +216,6 @@ public:
 public:
 	DECLARE_LIBID(LIBID_TControlSvrLib)
 	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_TCONTROLSVR, "{A666C778-2308-47B0-A6F4-AAE1B0BB50D8}")
-	HRESULT InitializeSecurity() throw()
-	{
-		// TODO : CoInitializeSecurity를 호출하고 서비스에 
-		// 올바른 보안 설정을
-		// 적용하십시오. PKT 수준 인증, 
-		// RPC_C_IMP_LEVEL_IDENTIFY 가장 수준 인증 
-		// 및 Null이 아닌 적절한 보안 설명자 등을 적용하면 됩니다.
-
-		return S_OK;
-	}
-	HRESULT RegisterAppId( bool bService = false) throw()
-	{
-		if(!Uninstall())
-			return E_FAIL;
-
-		HRESULT hr = UpdateRegistryAppId(TRUE);
-		if(FAILED(hr))
-			return hr;
-
-		CRegKey keyAppID;
-		LONG lRes = keyAppID.Open( HKEY_CLASSES_ROOT, _T("AppID"), KEY_WRITE);
-		if( lRes != ERROR_SUCCESS )
-			return AtlHresultFromWin32(lRes);
-
-		CRegKey key;
-		lRes = key.Create( keyAppID, GetAppIdT());
-
-		if( lRes != ERROR_SUCCESS )
-			return AtlHresultFromWin32(lRes);
-
-		key.DeleteValue(_T("LocalService"));
-		if(!bService)
-			return S_OK;
-		key.SetStringValue(_T("LocalService"), m_szServiceName);
-
-		// Create service
-		if(!Install())
-			return E_FAIL;
-
-		return S_OK;
-	}
-	bool ParseCommandLine( LPCTSTR lpCmdLine, HRESULT* pnRetCode) throw()
-	{
-		TCHAR szTokens[] = _T("-/");
-		*pnRetCode = S_OK;
-
-		LPCTSTR lpszToken = FindOneOf( lpCmdLine, szTokens);
-		while(lpszToken)
-		{
-			if( WordCmpI( lpszToken, _T("UnregServer")) == 0 )
-			{
-				*pnRetCode = UnregisterServer(TRUE);
-
-				if(SUCCEEDED(*pnRetCode))
-					*pnRetCode = UnregisterAppId();
-
-				return false;
-			}
-
-			// Register as Local Server
-			if( WordCmpI( lpszToken, _T("RegServer")) == 0 )
-			{
-				*pnRetCode = RegisterAppId();
-
-				if(SUCCEEDED(*pnRetCode))
-					*pnRetCode = RegisterServer(TRUE);
-
-				return false;
-			}
-
-			if( WordCmpI( lpszToken, _T("Service")) == 0 ||
-				WordCmpI( lpszToken, _T("Install")) == 0 )
-			{
-				*pnRetCode = RegisterAppId(true);
-
-				if(SUCCEEDED(*pnRetCode))
-					*pnRetCode = RegisterServer(TRUE);
-
-				return false;
-			}
-
-			if( WordCmpI( lpszToken, _T("UnInstall")) == 0 )
-			{
-				*pnRetCode = S_OK;
-				if(!Uninstall())
-					*pnRetCode = E_FAIL;
-
-				return false;
-			}
-
-			if( WordCmpI( lpszToken, _T("Name")) == 0 )
-			{
-				CString strNAME(FindOneOf( lpszToken, _T(" ")));
-				int nPOS = 0;
-
-				strNAME = strNAME.Tokenize( _T(" "), nPOS);
-				strcpy( m_szServiceName, LPCTSTR(strNAME));
-			}
-
-			lpszToken = FindOneOf( lpszToken, szTokens);
-		}
-
-		return true;
-	}
-	BOOL Install() throw()
-	{
-		if(IsInstalled())
-			return TRUE;
-
-		// Get the executable file path
-		TCHAR szFilePath[MAX_PATH + _ATL_QUOTES_SPACE];
-		DWORD dwFLen = ::GetModuleFileName(NULL, szFilePath + 1, MAX_PATH);
-		if( dwFLen == 0 || dwFLen == MAX_PATH )
-			return FALSE;
-
-		// Quote the FilePath before calling CreateService
-		szFilePath[0] = _T('\"');
-		szFilePath[dwFLen + 1] = _T('\"');
-		szFilePath[dwFLen + 2] = 0;
-
-		CString strFilePath;
-		strFilePath.Format( "%s -Name %s", szFilePath, m_szServiceName);
-
-		SC_HANDLE hSCM = ::OpenSCManager( NULL, NULL, SC_MANAGER_ALL_ACCESS);
-		if( hSCM == NULL )
-		{
-			TCHAR szBuf[1024];
-
-			if( AtlLoadString( ATL_SERVICE_MANAGER_OPEN_ERROR, szBuf, 1024) == 0 )
-				lstrcpy( szBuf,  _T("Could not open Service Manager"));
-			MessageBox( NULL, szBuf, m_szServiceName, MB_OK);
-
-			return FALSE;
-		}
-
-		SC_HANDLE hService = ::CreateService(
-			hSCM,
-			m_szServiceName,
-			m_szServiceName,
-			SERVICE_ALL_ACCESS,
-			SERVICE_WIN32_OWN_PROCESS,
-			SERVICE_DEMAND_START,
-			SERVICE_ERROR_NORMAL,
-			LPCSTR(strFilePath),
-			NULL, NULL,
-			_T("RPCSS\0"),
-			NULL, NULL);
-
-		if( hService == NULL )
-		{
-			::CloseServiceHandle(hSCM);
-			TCHAR szBuf[1024];
-
-			if( AtlLoadString( ATL_SERVICE_START_ERROR, szBuf, 1024) == 0 )
-				lstrcpy(szBuf,  _T("Could not start service"));
-			MessageBox( NULL, szBuf, m_szServiceName, MB_OK);
-
-			return FALSE;
-		}
-
-		::CloseServiceHandle(hService);
-		::CloseServiceHandle(hSCM);
-
-		return TRUE;
-	}
-	HRESULT PreMessageLoop( int nShowCmd);
-	HRESULT PostMessageLoop();
 };
 
 
--- TServer/TControlSvr/TControlSvr_i.c
+++ TServer/TControlSvr/TControlSvr_i.c
@@ -5,23 +5,19 @@
 /* link this file in with the server and any clients */
 
 
- /* File created by MIDL compiler version 6.00.0361 */
-/* at Wed Dec 30 20:22:16 2015
+ /* File created by MIDL compiler version 8.01.0622 */
+/* at Tue Jan 19 04:14:07 2038
  */
-/* Compiler settings for .\TControlSvr.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run)
+/* Compiler settings for TControlSvr.idl:
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.01.0622 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
          __declspec(uuid()), __declspec(selectany), __declspec(novtable)
          DECLSPEC_UUID(), MIDL_INTERFACE()
 */
-//@@MIDL_FILE_HEADING(  )
+/* @@MIDL_FILE_HEADING(  ) */
 
-#if !defined(_M_IA64) && !defined(_M_AMD64)
-
-
-#pragma warning( disable: 4049 )  /* more than 64k source lines */
 
 
 #ifdef __cplusplus
@@ -66,9 +62,9 @@ typedef IID CLSID;
 #endif // CLSID_DEFINED
 
 #define MIDL_DEFINE_GUID(type,name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
-        const type name = {l,w1,w2,{b1,b2,b3,b4,b5,b6,b7,b8}}
+        EXTERN_C __declspec(selectany) const type name = {l,w1,w2,{b1,b2,b3,b4,b5,b6,b7,b8}}
 
-#endif !_MIDL_USE_GUIDDEF_
+#endif // !_MIDL_USE_GUIDDEF_
 
 MIDL_DEFINE_GUID(IID, LIBID_TControlSvrLib,0x761E89DF,0x0BF0,0x4F94,0x91,0x43,0xBC,0x64,0xB2,0xF4,0x59,0xB8);
 
@@ -79,6 +75,4 @@ MIDL_DEFINE_GUID(IID, LIBID_TControlSvrLib,0x761E89DF,0x0BF0,0x4F94,0x91,0x43,0x
 #endif
 
 
-
-#endif /* !defined(_M_IA64) && !defined(_M_AMD64)*/
 
